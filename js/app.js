44435b996ff2aa0870b70487d3f3195472052232
jshprintz <jshprintz@gmail.com>
2022-07-13 16:14:00 -0700

    Wednesday Work

---
+++ js/app.js
@@ -0,0 +1,29 @@
+console.log('Javascript file is loaded');
+
+function computeArea(width, height) {   
+    return `The area of a rectangle with a width of ${width} and a height of ${height} is ${width*height} square units.`;
+}
+
+console.log(computeArea(25,634));
+
+
+
+// const multiply = (a,b) => a * b;
+// const divide = (a,b) => a / b;
+// const subtract = (a,b) => a - b;
+
+// console.log(multiply(5, 10)); //Returns 50
+// console.log(divide(10, 5)); //Returns 2
+// console.log(subtract(10, 5)); //Returns 5
+
+
+// fnDeclaration(); //Calling the function
+// fnExpression(); //Calling the function
+
+// function fnDeclaration(){
+//     console.log("I'm coming from a function declaration");
+// }
+
+// const fnExpression = function(){
+//     console.log("I'm coming from a function expression");
+// };
